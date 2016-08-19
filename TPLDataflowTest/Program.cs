1357d5140fa8f5fa22362337fbb8e2201b736ee4
Vladimir.Tyrin <b0-0b@yandex.ru>
2016-08-19 13:04:57 +0300

    Files added

--- TPLDataflowTest/Program.cs
+++ TPLDataflowTest/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using ITCC.Logging.Core;
+using ITCC.Logging.Windows.Loggers;
 
 namespace TPLDataflowTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
         {
+            InitLoggers();
+            LogMessage(LogLevel.Info, "Started");
+
+            await Task.Delay(1);
         }
+
+        private static void InitLoggers()
+        {
+            Logger.Level = LogLevel.Trace;
+            Logger.RegisterReceiver(new ColouredConsoleLogger());
+        }
+
+        private static void LogMessage(LogLevel level, string message) => Logger.LogEntry("TPL TEST", level, message);
     }
 }
